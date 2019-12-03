6e3189f7a9ace6b718f6d44e1049780fc18ca27b
Stephen Salinas <ssalinas@hubspot.com>
2019-12-03 14:11:14 -0500

    quieter logging

--- sources/src/main/java/com/ullink/slack/simpleslackapi/impl/SlackWebSocketSessionImpl.java
+++ sources/src/main/java/com/ullink/slack/simpleslackapi/impl/SlackWebSocketSessionImpl.java
@@ -237,7 +237,7 @@ public class SlackWebSocketSessionImpl extends AbstractSlackSessionImpl implemen
                     dispatchImpl((UserTyping) event, userTypingListener);
                     break;
                 default:
-                    LOGGER.warn("event of type " + event.getEventType() + " not handled: " + ((UnknownEvent)event).getJsonPayload());
+                    LOGGER.debug("event of type " + event.getEventType() + " not handled: " + ((UnknownEvent)event).getJsonPayload());
             }
         }
 
